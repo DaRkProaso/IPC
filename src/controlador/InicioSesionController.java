098d415912e9cee071d08aca6cd58641fb63e33e
Iguanator <imiguanator@protonmail.com>
2023-05-31 12:44:28 +0200

    fes me cas

--- src/controlador/InicioSesionController.java
+++ src/controlador/InicioSesionController.java
@@ -117,16 +117,7 @@ public class InicioSesionController implements Initializable {
             Image image = new Image("/imagenes/Icono.png");
             stage.getIcons().add(image);
             loginButton.getScene().getWindow().hide();
-            
-            stage.setResizable(true);
-            double minWidth = 650; // Ancho mínimo deseado
-            double minHeight = 450; // Alto mínimo deseado
-            double maxWidth = 850;
-            double maxHeight = 650;
-            stage.setMinWidth(minWidth); // Establecer ancho mínimo
-            stage.setMinHeight(minHeight); // Establecer alto mínimo
-            stage.setMaxWidth(maxWidth);
-            stage.setMaxHeight(maxHeight);
+            stage.setResizable(false); 
             stage.show();
         }
         else {warningLabel.setText("Usuario o contraseña incorecta");}
